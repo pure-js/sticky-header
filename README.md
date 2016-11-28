d5390e023bc2cd4bcf9fe1f927eda993396f1a40
pure-js <evolution.is.already.underway@gmail.com>
2016-11-28 23:11:19 +0300

    remove jquery

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 # Sticky header
-[jQuery](https://jquery.com/) plugin for sticky header
-
+Script for sticky header
 ## Installation
-```
-npm i -g mocha
-npm i -g jshint
-npm i -g bower
-```
-
-## For beginning
 ```
 bower install sticky-header --save
 mocha
 npm run jshint
+```
+## Contribution
+```
+npm i -g mocha
+npm i -g jshint
+npm i -g bower
 ```
--- src/sticky-header.js
+++ src/sticky-header.js
@@ -6,35 +6,20 @@
  * https://github.com/pure-jsy/sticky-header/blob/master/LICENSE
  */
 
+function stickyHeader({
+  el = document.querySelector('.header-fixed'),
+  offsetTop = 470,
+  hideClass = 'hidden'
+}) {
+  // Hide by default
+  hideClass ? el.classList.add(hideClass) : el.style.display = 'none';
 
-function stickyHeader() {
-  'use strict';
-
-  const defaults = {
-    fixedBlock: '.header_fixed',
-    offsetTop: 470,
-    speed: 50,
-    hideClassIsExist: true
-  };
-
-  return this.each(function () {
-
-    const settings = $.extend({}, defaults, options),
-      $fixedBlock = $(settings.fixedBlock, this),
-      el = document.querySelectorAll(settings.fixedBlock),
-      speed = settings.speed,
-      offsetTop = settings.offsetTop,
-      hideClassIsExist = settings.hideClassIsExist;
-
-    // Hide by default
-    hideClassIsExist ? el.classList.add('hide') : el.style.display = 'none';
-
-    $(window).scroll(function() {
-      if ( $(this).scrollTop() > offsetTop ) { // Show on scroll
-        $fixedBlock.slideDown( speed );
-      } else if ( $(this).scrollTop() <= offsetTop ) { // Hide on scroll
-        $fixedBlock.slideUp( speed );
-      }
-    });
+  window.addEventListener('scroll', function() {
+    const scrollTop = document.body.scrollTop;
+    if (scrollTop > offsetTop) {
+      el.classList.add(hideClass);
+    } else {
+      el.classList.remove(hideClass);
+    }
   });
 }
